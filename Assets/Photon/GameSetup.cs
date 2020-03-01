8395ffe17d630811f13846144a99bf88d347e6c3
Ryangraves93 <ryangraves1993@gmail.com>
2020-03-01 23:30:07 +0000

    Initial Commit

---
+++ Assets/Photon/GameSetup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class GameSetup : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatePlayer();
+    }
+
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating Player");
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
+    }
+
+}
+
+
